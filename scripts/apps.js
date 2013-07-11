7e3ba1c568e0e8ff39c5a4dd3f2cbfb9ba43ba5a
alexizentner <alexiz19001@yahoo.com>
2013-07-11 15:01:32 -0400

    revised

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,39 +1,39 @@
 $(document).ready(function() {
-	$('.pics-bc').on('mouseenter', function(){
+	$('.pics-bc').on('mouseenter', function(event){
 			event.preventDefault();
 			$('.banner').hide();
 			$(this).find('.arrow-left').show();
 			$('.bc-banner').show();
 	});	
 
-	$('.pics-bc').on('mouseleave', function(){
+	$('.pics-bc').on('mouseleave', function(event){
 			event.preventDefault();
 			$(this).find('.arrow-left').hide();
 			$('.bc-banner').hide();
 			$('.banner').show();
 	});	
-	$('.pics-hr').on('mouseenter', function(){
+	$('.pics-hr').on('mouseenter', function(event){
 			event.preventDefault();
 			$('.banner').hide();
 			$(this).find('.arrow-left2').show();
 			$('.hr-banner').show();
 	});	
 	
-		$('.pics-hr').on('mouseleave', function(){
+		$('.pics-hr').on('mouseleave', function(event){
 			event.preventDefault();
 			$(this).find('.arrow-left2').hide();
 			$('.hr-banner').hide();
 			$('.banner').show();
 	});	
 
-		$('.pics-cf').on('mouseenter', function(){
+		$('.pics-cf').on('mouseenter', function(event){
 			event.preventDefault();
 			$('.banner').hide();
 			$(this).find('.arrow-right').show();
 			$('.cf-banner').show();
 	});	
 	
-		$('.pics-cf').on('mouseleave', function(){
+		$('.pics-cf').on('mouseleave', function(event){
 			event.preventDefault();
 			$(this).find('.arrow-right').hide();
 			$('.cf-banner').hide();
